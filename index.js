68fdf6927b2892469841ac4326cac0b55b5d7c9a
Ridhima1999 <ridhimamehta99@gmail.com>
2020-07-24 15:12:33 +0530

    Resume view

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var app = express();
+app.use(express.static(__dirname+'/frontend'))
 app.get('/',function(req,res){
-    res.send('hello world!!');
+    res.sendFile(__dirname+'/frontend/html/index.html');
     
 })
+app.get('/resume',function(req,res){
+    res.sendFile(__dirname+'/frontend/html/resume.html')
+})
 
-var port= process.env.PORT||3003;
+
+var port= process.env.PORT||3000;
 app.listen(port,function(){
     console.log('site running on http:/localhost:'+port)
 });
